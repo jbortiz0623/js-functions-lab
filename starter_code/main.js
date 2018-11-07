c06d137e17b1d1ae1e21b876226210e0dd0ca663
jbortiz0623 <jbortiz0623@gmail.com>
2018-11-07 09:21:03 -0800

    answered Q&A

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,52 +1,95 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(a,b) {
+    if (a < b) {
+      return b;
+    } else if (a > b) {
+      return a;
+    }
 }
-
+  
 // Question 2
-function maxOfThree() {
-
+  function maxOfThree(a,b,c) {
+    if (a> b && b > c) {
+      return a;
+    }
+  
+    if (b > a && b > c) {
+      return b;
+    }
+  
+    if (c > a && c > b) {
+      return c;
+    }
 }
-
+  
 // Question 3
-function isCharacterAVowel() {
-
-}
-
+   function isCharacterAVowel(v) {
+    var vowels = ['a','e','i','o','u'];
+    if (vowels.indexOf(v) !== -1) {
+      return true;
+    } 
+    return false;
+  }
+  
 // Question 4
-function sumArray() {
- 
+  function sumArray(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
 }
-
-
+  
+  
 // Question 4
-function multiplyArray() {
-
+  function multiplyArray(arr) {
+  var product = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+  product *= arr[i];
+  }
+  return product;
 }
-
-
+  
+  
 // Question 5
-var numberOfArguments = function(){
+  var numberOfArguments = function(){
+    return arguments.length
+  }
   
-}
-
-
-
+  
+  
 // Question 6
-var reverseString = function (){
+  var reverseString = function (str){
+    var arr = str.split('');
+    var revArr = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+      revArr.push(arr[i]);
+    }
+    return revArr.join('');
+  };
   
-};
-
-
+  
 // Question 7
-function findLongestWord () {
+  function findLongestWord (string) {
+    longest = 0;
+    for (var i = 0; i < string.length; i++) {
+      if (string[i].length > longest) {
+        longest = string[i].length;
+      }
+    }
+    return longest;
+  }
   
-}
-
-
+  
 // Question 8
-function filterLongWords () {
-  
+  function filterLongWords (arr,num) {
+    newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      if(arr[i].length > num){
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
 }
 
 
